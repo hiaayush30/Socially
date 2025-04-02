202ec90a111e51e7c3f87f5903864567986739fd
aayush <hiaayush30@gmail.com>
2025-04-02 17:50:35 +0530

    commit

--- src/app/(protected)/profile/[...userId]/page.tsx
+++ src/app/(protected)/profile/[...userId]/page.tsx
@@ -63,7 +63,7 @@ export type UserType = Prisma.UserGetPayload<{
   }
 }>
 
-async function UserProfile({ params }: { params: { userId: string | string[] } }) {
+async function UserProfile({ params }: { params: any }) {
   const userId = Array.isArray(params.userId) ? params.userId[0] : params.userId;
   try {
     const foundUser = await prisma.user.findFirst({
