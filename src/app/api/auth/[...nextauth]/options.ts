58b70574c54739a77962c749ced7d36aee1d4131
aayush <hiaayush30@gmail.com>
2025-04-08 16:07:58 +0530

    updated options.ts

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,14 +3,9 @@ import prisma from "../../../../lib/db";
 import bcrypt from "bcrypt";
 import { NextAuthOptions } from "next-auth";
 import { UserInterface } from "@/types/schema";
-// import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
     providers: [
-        // GitHubProvider({
-        //     clientId: process.env.GITHUB_CLIENT_ID as string,
-        //     clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-        // }),
         CredentialsProvider({
             name: "Credentials",
             credentials: {
